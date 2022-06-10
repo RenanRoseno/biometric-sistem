f9d23381673543c00386fb60b0b6ef5b69f0f749
Renan Roseno <rosenorenan@gmail.com>
2022-06-10 10:10:17 -0300

    Add cleaning code

--- biometric-sistem.ino
+++ biometric-sistem.ino
@@ -2,95 +2,112 @@
 #include <NTPClient.h>
 #include <time.h>
 
-// set the LCD number of columns and rows
-const char* ssid = "--";
-const char* password = "--";
-int timezone = -3; //time offset
-int dst = 0;
+const char *ssid = "--";
+const char *password = "--";
+const int delay1000 = 1000;
+//Functions
+void initLCD();
+void clearLCD();
+void showTime();
 
+// Timer attributes
+int timezone = -3;
+int dst = 0;
+bool isBeforeEvening = false;
+
+// LCD fields attributes
 int lcdColumns = 16;
 int lcdRows = 2;
 
-bool isBeforeEvening = false;
+LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
 
-// set LCD address, number of columns and rows
-// if you don't know your display address, run an I2C scanner sketch
-LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);  
-
-void setup(){
-  // initialize LCD
+void setup()
+{
+  // LCD
   lcd.init();
-  // turn on LCD backlight                      
   lcd.backlight();
+
   Serial.begin(115200);
+
+  // WIFI
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.println("\nConnecting to WiFi");
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     Serial.print(".");
     delay(500);
   }
+
+  // Timer
   configTime(timezone * 3600, dst * 0, "pool.ntp.org", "time.nist.gov");
-  while (!time(nullptr)) {
-  Serial.print(".");
-  delay(1000);
+  while (!time(nullptr))
+  {
+    Serial.print(".");
+    delay(delay1000);
   }
 }
 
-void loop(){
+void loop()
+{
   initLCD();
   showTime();
 }
 
-void initLCD(){
+void initLCD()
+{
   time_t now;
-  struct tm * timeinfo;
+  struct tm *timeinfo;
   time(&now);
   timeinfo = localtime(&now);
+
   lcd.setCursor(0, 0);
   lcd.print("LEITOR BIOMETRICO");
-  char saudacao[] = "";
   lcd.setCursor(0, 1);
-  
-  if(timeinfo->tm_hour > 12){
+
+  if (timeinfo->tm_hour > 12)
+  {
     lcd.print("Boa tarde");
     isBeforeEvening = true;
-  }else {
-    if(isBeforeEvening){
+  }
+  else
+  {
+    if (isBeforeEvening)
+    {
       lcd.clear();
-     isBeforeEvening = false;
+      isBeforeEvening = false;
     }
-    lcd.print("Bom dia");   
+    lcd.print("Bom dia");
   }
 
   lcd.setCursor(0, 2);
-  lcd.printf("%02d:%02d:%02d", timeinfo->tm_hour, 
-           timeinfo->tm_min, timeinfo->tm_sec); 
-   
- 
+  lcd.printf("%02d:%02d:%02d", timeinfo->tm_hour,
+             timeinfo->tm_min, timeinfo->tm_sec);
 }
 
-void clearLCD(){
-   lcd.clear(); 
+void clearLCD()
+{
+  lcd.clear();
 }
 
-void showTime(){
-     time_t now;
-    struct tm * timeinfo;
-    time(&now);
-    //timeinfo = gmtime(&now); 
-    timeinfo = localtime(&now);
+void showTime()
+{
+  time_t now;
+  struct tm *timeinfo;
+  time(&now);
+  timeinfo = localtime(&now);
 
-    if(timeinfo->tm_hour < 12){
-      Serial.println("Bom dia");      
-    }else {
-      Serial.println("Boa tarde"); 
-    }
-    Serial.println(timeinfo->tm_hour);
-    Serial.printf("The time is: %02d:%02d:%02d\n", timeinfo->tm_hour, 
-           timeinfo->tm_min, timeinfo->tm_sec); 
-    //  , timeinfo->tm_mday, timeinfo->tm_mon+1, timeinfo->tm_year-100);
-     
-    //Serial.println(ctime(&now));
-    delay(1000);
+  if (timeinfo->tm_hour < 12)
+  {
+    Serial.println("Bom dia");
+  }
+  else
+  {
+    Serial.println("Boa tarde");
+  }
+  Serial.println(timeinfo->tm_hour);
+  Serial.printf("The time is: %02d:%02d:%02d\n", timeinfo->tm_hour,
+                timeinfo->tm_min, timeinfo->tm_sec);
+
+  delay(delay1000);
 }
