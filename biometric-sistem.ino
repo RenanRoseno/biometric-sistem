1e244e74b7fa63ae9560da29889f4d995183d421
Renan Roseno <rosenorenan@gmail.com>
2022-06-21 11:14:10 -0300

    Integrating modules

--- biometric-sistem.ino
+++ biometric-sistem.ino
@@ -2,6 +2,16 @@
 #include <NTPClient.h>
 #include <time.h>
 #include <Keypad.h>
+#include <Adafruit_Fingerprint.h> //https://github.com/adafruit/Adafruit-Fingerprint-Sensor-Library
+#include <IOXhop_FirebaseESP32.h>                           
+#include <ArduinoJson.h>      
+
+#define FIREBASE_HOST "https://biometric-sistem-default-rtdb.firebaseio.com/"    
+#define FIREBASE_AUTH "pyQfKSOtmaOfTHA0stGjnJwMolhpybh1U9ksfnOp"   
+
+//Senha padrão do sensor de digitais
+const uint32_t password = 0x0;
+Adafruit_Fingerprint fingerprintSensor = Adafruit_Fingerprint(&Serial2, password);
 
   
 const byte n_rows = 4;
@@ -26,8 +36,8 @@ Keypad myKeypad = Keypad( makeKeymap(keys), rowPins, colPins, n_rows, n_cols);
 // set the LCD number of columns and rows
 
 
-const char* ssid = "--";
-const char* password = "--";
+const char* ssid = "HotnetNayane";
+const char* passwordW = "@.NR1402";
 int timezone = -3; //time offset
 int dst = 0;
 
@@ -41,13 +51,14 @@ bool isBeforeEvening = false;
 LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);  
 
 void setup(){
-  // initialize LCD
-  //lcd.init();
+//   initialize LCD
+  lcd.init();
   // turn on LCD backlight                      
-  //lcd.backlight();
-  Serial.begin(115200);
-  /*WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
+  lcd.backlight();
+    setupFingerprintSensor();
+  Serial.begin(9600);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, passwordW);
   Serial.println("\nConnecting to WiFi");
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
@@ -57,14 +68,56 @@ void setup(){
   while (!time(nullptr)) {
   Serial.print(".");
   delay(1000);
-  }*/
+  }
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 }
 
 void loop(){
-  //initLCD();
+  initLCD();
   //showTime();
-  showKeyPressed();
+  //showKeyPressed();
+  /*
+   int i = command.toInt();
+
+  //Verifica qual o número digitado e chama a função correspondente
+  switch (i)
+  {
+    case 1:
+      storeFingerprint();
+      break;
+    case 2:
+      checkFingerprint();
+      break;
+    case 3:
+      printStoredFingerprintsCount();
+      break;
+    case 4:
+      deleteFingerprint();
+      break;
+    case 5:
+      emptyDatabase();
+      break;
+    default:
+      Serial.println(F("Opção inválida"));
+      break;
+  }
+   */
 }
+
+void setupFingerprintSensor()
+{
+  //Inicializa o sensor
+  fingerprintSensor.begin(57600);
+
+  //Verifica se a senha está correta
+  if(!fingerprintSensor.verifyPassword())
+  {
+    //Se chegou aqui significa que a senha está errada ou o sensor está problemas de conexão
+    Serial.println(F("Não foi possível conectar ao sensor. Verifique a senha ou a conexão"));
+    while(true);
+  }
+}
+
 
 void initLCD(){
   time_t now;
@@ -72,22 +125,22 @@ void initLCD(){
   time(&now);
   timeinfo = localtime(&now);
   lcd.setCursor(0, 0);
-  lcd.print("LEITOR BIOMETRICO");
+  lcd.print("  LEITOR BIOMETRICO");
   char saudacao[] = "";
   lcd.setCursor(0, 1);
   
   if(timeinfo->tm_hour > 12){
-    lcd.print("Boa tarde");
+    lcd.print("Boa tarde! ");
     isBeforeEvening = true;
   }else {
     if(isBeforeEvening){
       lcd.clear();
      isBeforeEvening = false;
     }
-    lcd.print("Bom dia");   
+      lcd.print("  Bom dia! ");   
   }
 
-  lcd.setCursor(0, 2);
+  //lcd.setCursor(0, 2);
   lcd.printf("%02d:%02d:%02d", timeinfo->tm_hour, 
            timeinfo->tm_min, timeinfo->tm_sec); 
    
@@ -125,5 +178,179 @@ void showKeyPressed(){
   if (myKey != NULL){
     Serial.print("Key pressed: ");
     Serial.println(myKey);
+  }
+}
+void storeFingerprint()
+{
+  Serial.println(F("Qual a posição para guardar a digital? (1 a 149)"));
+
+  //Lê o que foi digitado no monitor serial
+ // String strLocation = getCommand();
+
+  //Transforma em inteiro
+  int location = 1; //strLocation.toInt();
+
+  //Verifica se a posição é válida ou não
+  if(location < 1 || location > 149)
+  {
+    //Se chegou aqui a posição digitada é inválida, então abortamos os próximos passos
+    Serial.println(F("Posição inválida"));
+    return;
+  }
+
+  Serial.println(F("Encoste o dedo no sensor"));
+
+  //Espera até pegar uma imagem válida da digital
+  while (fingerprintSensor.getImage() != FINGERPRINT_OK);
+  
+  //Converte a imagem para o primeiro padrão
+  if (fingerprintSensor.image2Tz(1) != FINGERPRINT_OK)
+  {
+    //Se chegou aqui deu erro, então abortamos os próximos passos
+    Serial.println(F("Erro image2Tz 1"));
+    return;
+  }
+  
+  Serial.println(F("Tire o dedo do sensor"));
+
+  delay(2000);
+
+  //Espera até tirar o dedo
+  while (fingerprintSensor.getImage() != FINGERPRINT_NOFINGER);
+
+  //Antes de guardar precisamos de outra imagem da mesma digital
+  Serial.println(F("Encoste o mesmo dedo no sensor"));
+
+  //Espera até pegar uma imagem válida da digital
+  while (fingerprintSensor.getImage() != FINGERPRINT_OK);
+
+  //Converte a imagem para o segundo padrão
+  if(fingerprintSensor.image2Tz(2) != FINGERPRINT_OK)
+  {
+    //Se chegou aqui deu erro, então abortamos os próximos passos
+    Serial.println(F("Erro image2Tz 2"));
+    return;
+  }
+
+  //Cria um modelo da digital a partir dos dois padrões
+  if(fingerprintSensor.createModel() != FINGERPRINT_OK)
+  {
+    //Se chegou aqui deu erro, então abortamos os próximos passos
+    Serial.println(F("Erro createModel"));
+    return;
+  }
+
+  //Guarda o modelo da digital no sensor
+  if(fingerprintSensor.storeModel(location) != FINGERPRINT_OK)
+  {
+    //Se chegou aqui deu erro, então abortamos os próximos passos
+    Serial.println(F("Erro storeModel"));
+    return;
+  }
+
+  //Se chegou aqui significa que todos os passos foram bem sucedidos
+  Serial.println(F("Sucesso!!!"));
+}
+
+//Verifica se a digital está cadastrada
+void checkFingerprint()
+{
+  Serial.println(F("Encoste o dedo no sensor"));
+
+  //Espera até pegar uma imagem válida da digital
+  while (fingerprintSensor.getImage() != FINGERPRINT_OK);
+
+  //Converte a imagem para o padrão que será utilizado para verificar com o banco de digitais
+  if (fingerprintSensor.image2Tz() != FINGERPRINT_OK)
+  {
+    //Se chegou aqui deu erro, então abortamos os próximos passos
+    Serial.println(F("Erro image2Tz"));
+    return;
+  }
+
+  //Procura por este padrão no banco de digitais
+  if (fingerprintSensor.fingerFastSearch() != FINGERPRINT_OK)
+  {
+    //Se chegou aqui significa que a digital não foi encontrada
+    Serial.println(F("Digital não encontrada"));
+    return;
+  }
+
+  //Se chegou aqui a digital foi encontrada
+  //Mostramos a posição onde a digital estava salva e a confiança
+  //Quanto mais alta a confiança melhor
+  Serial.print(F("Digital encontrada com confiança de "));
+  Serial.print(fingerprintSensor.confidence);
+  Serial.print(F(" na posição "));
+  Serial.println(fingerprintSensor.fingerID);
+}
+
+void printStoredFingerprintsCount()
+{
+  //Manda o sensor colocar em "templateCount" a quantidade de digitais salvas
+  fingerprintSensor.getTemplateCount();
+
+  //Exibe a quantidade salva
+  Serial.print(F("Digitais cadastradas: "));
+  Serial.println(fingerprintSensor.templateCount);
+}
+
+void deleteFingerprint()
+{
+  Serial.println(F("Qual a posição para apagar a digital? (1 a 149)"));
+
+  //Lê o que foi digitado no monitor serial
+  String strLocation = "1";
+
+  //Transforma em inteiro
+  int location = strLocation.toInt();
+
+  //Verifica se a posição é válida ou não
+  if(location < 1 || location > 149)
+  {
+    //Se chegou aqui a posição digitada é inválida, então abortamos os próximos passos
+    Serial.println(F("Posição inválida"));
+    return;
+  }
+
+  //Apaga a digital nesta posição
+  if(fingerprintSensor.deleteModel(location) != FINGERPRINT_OK)
+  {
+    Serial.println(F("Erro ao apagar digital"));
+  }
+  else
+  {
+    Serial.println(F("Digital apagada com sucesso!!!"));
+  }
+}
+
+void emptyDatabase()
+{
+  Serial.println(F("Tem certeza? (s/N)"));
+
+  //Lê o que foi digitado no monitor serial
+//  String command = getCommand();
+
+  //Coloca tudo em maiúsculo para facilitar a comparação
+ String  command = "S"; //.toUpperCase();
+
+  //Verifica se foi digitado "S" ou "SIM"
+  if(command == "S" || command == "SIM")
+  {
+    Serial.println(F("Apagando banco de digitais..."));
+
+    //Apaga todas as digitais
+    if(fingerprintSensor.emptyDatabase() != FINGERPRINT_OK)
+    {
+      Serial.println(F("Erro ao apagar banco de digitais"));
+    }
+    else
+    {
+      Serial.println(F("Banco de digitais apagado com sucesso!!!"));
+    }
+  }
+  else
+  {
+    Serial.println(F("Cancelado"));
   }
 }
